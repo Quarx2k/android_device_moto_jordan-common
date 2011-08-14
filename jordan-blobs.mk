5472484d36abf5240b5880a19337ee0a6b981a7c
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-08-14 16:58:12 +0200

    dnsconf name is not same

--- jordan-blobs.mk
+++ jordan-blobs.mk
@@ -20,7 +20,7 @@ PRODUCT_COPY_FILES += \
 	device/motorola/jordan/recovery.fstab:system/etc/recovery.fstab \
 	device/motorola/jordan/prebuilt/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
 	device/motorola/jordan/prebuilt/etc/init.d/05mountsd:system/etc/init.d/05mountsd \
-	device/motorola/jordan/prebuilt/etc/init.d/10-dnsconf:system/etc/init.d/10-dnsconf \
+	device/motorola/jordan/prebuilt/etc/init.d/10dnsconf:system/etc/init.d/10dnsconf \
 	device/motorola/jordan/prebuilt/etc/init.d/90multitouch:system/etc/init.d/90multitouch \
 	device/motorola/jordan/prebuilt/etc/profile:system/etc/profile \
 	device/motorola/jordan/prebuilt/etc/sysctl.conf:system/etc/sysctl.conf \
