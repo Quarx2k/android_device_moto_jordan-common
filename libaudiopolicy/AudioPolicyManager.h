8f3410b79cae9f2dee9e3557888bcdcc1c6b535a
Quarx2k <agent00791@gmail.com>
2011-10-18 15:21:41 +0700

    fix

--- libaudiopolicy/AudioPolicyManager.h
+++ libaudiopolicy/AudioPolicyManager.h
@@ -42,15 +42,6 @@ protected:
         virtual bool a2dpUsedForSonification() const { return true; }
 #endif
 
-
-#ifdef HAVE_FM_RADIO
-
-        virtual status_t setDeviceConnectionState(AudioSystem::audio_devices device,
-                                                  AudioSystem::device_connection_state state,
-                                                  const char *device_address);
-
-        void resetFm(AudioSystem::audio_devices device);
-
 #ifdef OMAP_ENHANCEMENT
 	virtual status_t setFMRxActive(bool state);
 #endif
