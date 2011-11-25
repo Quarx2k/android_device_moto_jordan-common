5118f51f63d5c9c1edf0b787fe9b8b507c41127a
Quarx2k <agent00791@gmail.com>
2011-11-26 01:03:36 +0700

    audio_policy.jordan.so

---
+++ audio_policy/Android.mk
@@ -0,0 +1,21 @@
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),jordan)
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= AudioPolicyManager.cpp
+LOCAL_SHARED_LIBRARIES:= libc libcutils libutils libmedia
+LOCAL_STATIC_LIBRARIES := libmedia_helper
+LOCAL_WHOLE_STATIC_LIBRARIES:= libaudiopolicy_legacy
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE:= audio_policy.jordan
+LOCAL_MODULE_TAGS := optional
+
+ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+  LOCAL_CFLAGS += -DWITH_A2DP
+endif
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
