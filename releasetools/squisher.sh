5cfe2001cc08667ac30d614f93d2f006ed17e0e4
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-11-29 13:14:40 +0100

    Fix for RIL audio

--- releasetools/squisher.sh
+++ releasetools/squisher.sh
@@ -37,6 +37,9 @@ cp -f $ANDROID_BUILD_TOP/out/target/product/jordan/utilities/busybox $REPACK/ota
 #cp -f $REPACK/ota/system/lib/hw/hwcomposer.default.so $REPACK/ota/system/lib/hw/hwcomposer.jordan.so
 #cp -f $REPACK/ota/system/lib/hw/gralloc.default.so $REPACK/ota/system/lib/hw/gralloc.omap3.so
 
+# ril fix
+cp -f $REPACK/ota/system/lib/hw/audio.a2dp.default.so $REPACK/ota/system/lib/liba2dp.so
+
 echo "0 0 android" > $REPACK/ota/system/lib/egl/egl.cfg
 echo "0 1 POWERVR_SGX530_125" >> $REPACK/ota/system/lib/egl/egl.cfg
 
