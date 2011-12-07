bb54d59497660f5657460360b554622d6acc6f70
Quarx2k <agent00791@gmail.com>
2011-12-07 20:01:42 +0700

    Quarx new camera library for the future... (squashed commits)

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -2,7 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES:= JordanCameraWrapper.cpp
+#LOCAL_SRC_FILES:= JordanCameraWrapper.cpp
+LOCAL_SRC_FILES:= JordanCameraWrapper_test.cpp
 
 LOCAL_SHARED_LIBRARIES:= libdl libutils libcutils libcamera_client
 
