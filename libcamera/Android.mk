736bf39ed7d60b5faf4b05aafd9bbc131a010ff2
Quarx2k <agent00791@gmail.com>
2011-12-08 22:09:23 +0700

    camera.jordan.so & libcamera.so in one Android.mk

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),jordan)
+
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -12,3 +14,29 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := camera.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libutils \
+    libmedia \
+    libcamera_client \
+    libbinder \
+    libhardware_legacy
+
+LOCAL_SHARED_LIBRARIES += libdl
+
+LOCAL_SHARED_LIBRARIES += libcamera
+LOCAL_SRC_FILES := hmi.c CameraHardwareInterfaceShim.cpp
+
+
+LOCAL_STATIC_LIBRARIES := \
+    libmedia_helper
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
