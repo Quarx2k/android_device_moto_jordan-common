a29f9da194bb736497ee4d35f1f69d85d1ccc396
Quarx2k <agent00791@gmail.com>
2012-02-29 20:48:40 +0700

    Revert "4vsel in ics"
    This reverts commit 5a9a493236c79a2992a94210781dce3709179b61.

--- bootmenu/script/overclock.sh
+++ bootmenu/script/overclock.sh
@@ -1,4 +1,4 @@
-#!/system/bin/sh
+#!/system/bootmenu/binary/busybox ash
 
 ######## BootMenu Script
 ######## Overclock.sh
@@ -166,16 +166,14 @@ set_scaling()
 
 set_overclock_table()
 {
-  echo "$vsel4" > /proc/overclock/max_vsel
-  echo "${clk4}000" > /proc/overclock/max_rate
-  echo "4 ${clk4}000000 $vsel4" > /proc/overclock/mpu_opps
+  echo "$vsel3" > /proc/overclock/max_vsel
+  echo "${clk3}000" > /proc/overclock/max_rate
   echo "3 ${clk3}000000 $vsel3" > /proc/overclock/mpu_opps
   echo "2 ${clk2}000000 $vsel2" > /proc/overclock/mpu_opps
   echo "1 ${clk1}000000 $vsel1" > /proc/overclock/mpu_opps
-  echo "0 ${clk4}000" > /proc/overclock/freq_table
-  echo "1 ${clk3}000" > /proc/overclock/freq_table
-  echo "2 ${clk2}000" > /proc/overclock/freq_table
-  echo "3 ${clk1}000" > /proc/overclock/freq_table
+  echo "0 ${clk3}000" > /proc/overclock/freq_table
+  echo "1 ${clk2}000" > /proc/overclock/freq_table
+  echo "2 ${clk1}000" > /proc/overclock/freq_table
 }
 
 #############################################################
