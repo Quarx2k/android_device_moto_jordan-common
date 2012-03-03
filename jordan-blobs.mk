22be0d1360af17170093fbdde491733c0cc601b4
Quarx2k <agent00791@gmail.com>
2012-03-03 20:38:28 +0700

    for defy no need prebuilt iptables, ics version works wtf%)

--- jordan-blobs.mk
+++ jordan-blobs.mk
@@ -59,7 +59,6 @@ PRODUCT_COPY_FILES += \
 	$(DEVICE_PREBUILT)/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
 	$(DEVICE_PREBUILT)/etc/gpsconfig.xml:system/etc/gpsconfig.xml \
 	$(DEVICE_PREBUILT)/etc/location.cfg:system/etc/location.cfg \
-	$(DEVICE_PREBUILT)/bin/iptables:system/bin/iptables \
 	device/motorola/jordan/vold.fstab:system/etc/vold.fstab \
 	device/motorola/jordan/media_profiles.xml:system/etc/media_profiles.xml \
 	device/motorola/jordan/modules/modules.alias:system/lib/modules/modules.alias \
