f1f7d5504d10818b32eccc8ac895cea743122856
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-04-11 09:02:14 +0200

    force libsensor cleanup
    Change-Id: I98c296fff1bead812a65ec8834536d40eda277e4

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -46,6 +46,9 @@
 
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/DefyParts_intermediates)
 
+# For the sensors lib, its better to rebuild it on defy/defy+ switch
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/sensors.jordan_intermediates)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
