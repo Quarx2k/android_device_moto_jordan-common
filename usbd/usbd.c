222c6fc98d40287a47f1eb58145fc46b3114c707
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-04-24 09:28:09 +0200

    tabs cleanup
    Change-Id: I7fd7ca12505046da78ca9e6f6d9fa4b329467bc0

--- usbd/usbd.c
+++ usbd/usbd.c
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2011 - 2012 Skrilax_CZ
  * Based on Motorola Usb daemon
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -156,44 +156,44 @@ enum usbd_state_t
 #define USB_MODE_CHARGE_ADB           "charge_adb"
 
 /* available modes */
-static struct usb_mode_info usb_modes[] = 
+static struct usb_mode_info usb_modes[] =
 {
 	USB_MODE_NONE,
-	
+
 	/* Factory */
 	USB_MODE_INFO(USB_MODE_ETH),
-	
+
 	/* Modem */
 	USB_MODE_INFO(USB_MODE_ACM),
-	
+
 	/* USBNet + Modem */
 	USB_MODE_INFO(USB_MODE_ACM_ETH),
 	USB_MODE_INFO_ADB(USB_MODE_ACM_ETH),
-	
+
 	/* USBNet + Modem + Mtp */
 	USB_MODE_INFO(USB_MODE_ACM_ETH_MTP),
 	USB_MODE_INFO_ADB(USB_MODE_ACM_ETH_MTP),
-	
+
 	/* Mtp */
 	USB_MODE_INFO(USB_MODE_MTP),
 	USB_MODE_INFO_ADB(USB_MODE_MTP),
-	
+
 	/* Mass storage mode */
 	USB_MODE_INFO(USB_MODE_MSC),
 	USB_MODE_INFO_ADB(USB_MODE_MSC),
-	
+
 	/* Charge only mode */
-	{ 
-		.mode =         USB_MODE_PREFIX        USB_MODE_CHARGE_ONLY, 
-		.start =        USBD_START_PREFIX      USB_MODE_CHARGE_ONLY, 
-		.req_switch =   "" 
+	{
+		.mode =         USB_MODE_PREFIX        USB_MODE_CHARGE_ONLY,
+		.start =        USBD_START_PREFIX      USB_MODE_CHARGE_ONLY,
+		.req_switch =   ""
 	},
-	{ 
-		.mode =         USB_MODE_PREFIX        USB_MODE_CHARGE_ADB, 
-		.start =        USBD_START_PREFIX      USB_MODE_CHARGE_ONLY, 
-		.req_switch =   "" 
+	{
+		.mode =         USB_MODE_PREFIX        USB_MODE_CHARGE_ADB,
+		.start =        USBD_START_PREFIX      USB_MODE_CHARGE_ONLY,
+		.req_switch =   ""
 	},
-	
+
 	/* RNDIS */
 	USB_MODE_INFO(USB_MODE_RNDIS),
 	USB_MODE_INFO_ADB(USB_MODE_RNDIS),
@@ -218,31 +218,31 @@ static int open_uevent_socket(void)
 {
 	struct sockaddr_nl addr;
 	int sz = 64*1024;
-	
+
 	memset(&addr, 0, sizeof(addr));
 	addr.nl_family = AF_NETLINK;
 	addr.nl_pid = getpid();
 	addr.nl_groups = 0xFFFFFFFF;
-	
+
 	uevent_fd = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_KOBJECT_UEVENT);
 	if (uevent_fd < 0)
 	{
 		LOGE("%s(): Unable to create uevent socket '%s'\n", __func__, strerror(errno));
 		return -1;
 	}
-	
-	if (setsockopt(uevent_fd, SOL_SOCKET, SO_RCVBUFFORCE, &sz, sizeof(sz)) < 0) 
+
+	if (setsockopt(uevent_fd, SOL_SOCKET, SO_RCVBUFFORCE, &sz, sizeof(sz)) < 0)
 	{
 		LOGE("%s(): Unable to set uevent socket options '%s'\n", __func__, strerror(errno));
 		return -1;
 	}
-	
+
 	if (bind(uevent_fd, (struct sockaddr *) &addr, sizeof(addr)) < 0)
 	{
 		LOGE("%s(): Unable to bind uevent socket '%s'\n", __func__, strerror(errno));
 		return -1;
 	}
-	
+
 	return 0;
 }
 
@@ -250,19 +250,19 @@ static int open_uevent_socket(void)
 static int init_usdb_socket()
 {
 	usbd_socket_fd = android_get_control_socket("usbd");
-	
+
 	if (usbd_socket_fd < 0)
 	{
 		LOGE("%s(): Obtaining file descriptor socket 'usbd' failed: %s\n", __func__, strerror(errno));
 		return 1;
 	}
-	
+
 	if (listen(usbd_socket_fd, 4) < 0)
 	{
 		LOGE("%s(): Unable to listen on fd '%d' for socket 'usbd': %s", __func__, usbd_socket_fd, strerror(errno));
 		return 1;
 	}
-	
+
 	return 0;
 }
 
@@ -270,19 +270,19 @@ static int init_usdb_socket()
 static int get_highest_socket_fd(void)
 {
 	int fd;
-	
+
 	if (usbd_app_fd >= 0)
 		fd = usbd_app_fd;
-	
+
 	if (usbd_socket_fd > fd)
 		fd = usbd_socket_fd;
-	
+
 	if (usb_device_fd > fd)
 		fd = usb_device_fd;
-	
+
 	if (uevent_fd > fd)
 		fd = uevent_fd;
-	
+
 	return fd;
 }
 
@@ -291,11 +291,11 @@ static int get_adb_enabled_status(void)
 {
 	char buff[PROPERTY_VALUE_MAX];
 	int ret;
-	
+
 	ret = property_get(PROPERTY_ADB_ENABLED, buff, "0");
 	if (!ret)
 		return -1;
-	
+
 	return (!strcmp(buff, "1"));
 }
 
@@ -303,19 +303,18 @@ static int get_adb_enabled_status(void)
 static int usbd_send_adb_status(int sockfd, int status)
 {
 	int ret;
-	
+
 	if (status == 1)
 	{
 		LOGI("%s(): Send ADB Enable message\n", __func__);
 		ret = write(sockfd, USBD_ADB_STATUS_ON, strlen(USBD_ADB_STATUS_ON) + 1);
-		
 	}
 	else
 	{
 		LOGI("%s(): Send ADB Disable message\n", __func__);
 		ret = write(sockfd, USBD_ADB_STATUS_OFF, strlen(USBD_ADB_STATUS_OFF) + 1);
 	}
-	
+
 	return ret <= 0; /*1 = fail */
 }
 
@@ -323,7 +322,7 @@ static int usbd_send_adb_status(int sockfd, int status)
 static int usbd_get_mode_index(const char* mode, enum usb_mode_get_t usbmod)
 {
 	size_t i;
-	
+
 	for (i = 0; i < ARRAY_SIZE(usb_modes); i++)
 	{
 		switch (usbmod)
@@ -332,23 +331,23 @@ static int usbd_get_mode_index(const char* mode, enum usb_mode_get_t usbmod)
 				if (!strcmp(mode, usb_modes[i].mode))
 					return i;
 				break;
-				
+
 			case USBMOD_START:
 				if (!strcmp(mode, usb_modes[i].start))
 					return i;
 				break;
-				
+
 			case USBMOD_REQ_SWITCH:
 				if (!strcmp(mode, usb_modes[i].req_switch))
 					return i;
 				break;
-				
+
 			default:
 				LOGE("%s(): %d is not valid usb mode type\n", __func__, usbmod);
 				return -1;
 		}
 	}
-	
+
 	LOGE("%s(): %s is not valid usb mode\n", __func__, mode);
 	return -1;
 }
@@ -359,20 +358,20 @@ static int usbd_set_usb_mode(int new_mode)
 	int adb_sts;
 	const char* current_mode_str;
 	const char* mode_str;
-	
+
 	if (new_mode > 0 && new_mode < ((int) ARRAY_SIZE(usb_modes)))
 	{
 		/* Kernel gadget driver doesn't have usb_mode_ prefix */
 		mode_str = &(usb_modes[new_mode].mode[strlen(USB_MODE_PREFIX)]);
 		current_mode_str = &(usb_modes[usb_current_mode].mode[strlen(USB_MODE_PREFIX)]);
-		
+
 		/* Motorola pigness as usual:
 		 * If we're changing to charge only mode we send mass storage mode to the kernel.
 		 * If we're in mass storage mode and want to change to charge only mode or vice versa
 		 * don't send anything to kernel (no change). The change has to be handled directly by usbd, 
 		 * so return 1 for direct change and send start message right away.
 		 */
-		
+
 		if ( (!strcmp(mode_str, USB_MODE_CHARGE_ONLY) && !strcmp(current_mode_str, USB_MODE_MSC)) ||
 			(!strcmp(mode_str, USB_MODE_MSC) && !strcmp(current_mode_str, USB_MODE_CHARGE_ONLY)) ||
 			
@@ -384,18 +383,18 @@ static int usbd_set_usb_mode(int new_mode)
 			LOGI("%s(): handling on my own, not alerting kernel\n", __func__);
 			return 1;
 		}
-		
+
 		if (!strcmp(mode_str, USB_MODE_CHARGE_ONLY))
 			mode_str = USB_MODE_MSC;
 		else if (!strcmp(mode_str, USB_MODE_CHARGE_ADB))
 			mode_str = USB_MODE_MSC USB_MODE_ADB_SUFFIX;
-		
+
 		if (write(usb_device_fd, mode_str, strlen(mode_str) + 1) < 0)
 		{
 			LOGE("%s(): Socket Write Failure: %s\n", __func__, strerror(errno));
 			return -1;
 		}
-		
+
 		usb_current_mode = new_mode;
 		LOGI("%s(): new_mode: %s\n", __func__, mode_str);
 		return 0;
@@ -418,16 +417,16 @@ static int usbd_get_cable_status(void)
 {
 	char buffer[256];
 	FILE* f;
-	
+
 	/* get cable type */
 	f = fopen(USB_MODEL_NAME_PATH, "r");
-	
+
 	if (!f)
 	{
 		LOGE("%s: Unable to open power_supply model_name file '%s'\n", __func__, strerror(errno));
 		return -errno;
 	}
-	
+
 	/* Read it */
 	if (!fgets(buffer, ARRAY_SIZE(buffer), f))
 	{
@@ -435,11 +434,11 @@ static int usbd_get_cable_status(void)
 		LOGE("%s: Unable to read power supply model_name for cable type\n", __func__);
 		return -EIO;
 	}
-	
+
 	/* Remove linefeed */
 	buffer[strlen(buffer) - 1] = '\0';
 	LOGI("%s(): cable_type = %s\n", __func__, buffer);
-	
+
 	if (!strcmp(buffer, USB_INPUT_CABLE_NORMAL))
 		usb_factory_cable = 0;
 	else if (!strcmp(buffer, USB_INPUT_CABLE_FACTORY))
@@ -447,18 +446,18 @@ static int usbd_get_cable_status(void)
 		usb_factory_cable = 1;
 		usbd_set_usb_mode(usbd_get_mode_index(USB_MODE_ETH, USBMOD_MODE));
 	}
-	
+
 	fclose(f);
-	
+
 	/* get online status */
 	f = fopen(USB_ONLINE_PATH, "r");
-	
+
 	if (!f)
 	{
 		LOGE("%s: Unable to open power_supply online file '%s'\n", __func__, strerror(errno));
 		return -errno;
 	}
-	
+
 	/* Read it */
 	if (!fgets(buffer, ARRAY_SIZE(buffer), f))
 	{
@@ -466,7 +465,7 @@ static int usbd_get_cable_status(void)
 		LOGE("%s: Unable to read power supply online stat\n", __func__);
 		return -EIO;
 	}
-	
+
 	/* Remove linefeed */
 	buffer[strlen(buffer) - 1] = '\0';
 	if (!strcmp(buffer, "1"))
@@ -479,10 +478,10 @@ static int usbd_get_cable_status(void)
 		usb_online = 0;
 		usb_state = USBDSTAT_CABLE_DISCONNECTED;
 	}
-	
+
 	LOGI("%s(): current usb_online = %d\n", __func__, usb_online);
 	fclose(f);
-	
+
 	return 0;
 }
 
@@ -490,30 +489,30 @@ static int usbd_get_cable_status(void)
 static int usbd_notify_current_status(int sockfd)
 {
 	const char* event_msg = NULL;
-	
+
 	/* No, don't send anything in case of factory cable */
 	if (usb_factory_cable)
 		return 0;
-	
+
 	switch(usb_state)
 	{
 		case USBDSTAT_CABLE_DISCONNECTED:
 			event_msg = USBD_EVENT_CABLE_DISCONNECTED;
 			break;
-			
+
 		case USBDSTAT_CABLE_CONNECTED:
 			event_msg = USBD_EVENT_CABLE_CONNECTED;
 			break;
-			
+
 		case USBDSTAT_GET_DESCRIPTOR:
 			event_msg = USBD_EVENT_GET_DESCRIPTOR;
 			break;
-			
+
 		case USBDSTAT_USB_ENUMERATED:
 			event_msg = USBD_EVENT_USB_ENUMERATED;
 			break;
 	}
-	
+
 	if (event_msg)
 	{
 		LOGI("%s(): Notifying App with Current Status : %s\n", __func__, event_msg);
@@ -523,16 +522,16 @@ static int usbd_notify_current_status(int sockfd)
 			return 1;
 		}
 	}
-	
+
 	last_sent_usb_online = usb_online;
 	return 0;
 }
 
 /* send usb mode to the Usb.apk */
 static int usbd_enum_process(int sockfd)
-{	
+{
 	LOGI("%s(): current usb mode = %d\n", __func__, usb_current_mode);
-	
+
 	if (write(sockfd, usb_modes[usb_current_mode].start, strlen(usb_modes[usb_current_mode].start) + 1) < 0)
 	{
 		LOGE("%s(): Socket Write Failure: %s\n", __func__, strerror(errno));
@@ -543,7 +542,6 @@ static int usbd_enum_process(int sockfd)
 		LOGI("%s(): enum done\n", __func__);
 		return 0;
 	}
-	
 }
 
 /* socket event */
@@ -552,10 +550,10 @@ static int usbd_socket_event(int sockfd)
 	char buffer[1024];
 	const char* suffix;
 	int res, new_mode;
-	
+
 	memset(buffer, 0, sizeof(buffer));
 	res = read(sockfd, buffer, ARRAY_SIZE(buffer));
-	
+
 	if (res < 0)
 	{
 		LOGE("%s(): Socket Read Failure: %s", __func__, strerror(errno));
@@ -565,34 +563,34 @@ static int usbd_socket_event(int sockfd)
 	{
 		LOGI("%s(): received %s\n", __func__, buffer);
 		new_mode = usbd_get_mode_index(buffer, USBMOD_MODE);
-		
+
 		if (new_mode < 0)
 		{
 			LOGE("%s(): %s is not valid usb mode\n", __func__, buffer);
 			return 1;
 		}
-		
+
 		LOGI("%s(): Matched new usb mode = %d , current mode = %d\n", __func__, new_mode, usb_current_mode);
-		
+
 		if (!new_mode)
 		{
 			usbd_set_usb_mode(0);
 			return 0;
 		}
-		
+
 		if (new_mode == -1)
 			suffix = USBD_RESP_FAIL;
 		else
 			suffix = USBD_RESP_OK;
-		
+
 		sprintf(buffer, "%s%s", usb_modes[new_mode].mode, suffix);
-		
+
 		if (write(sockfd, buffer, strlen(buffer) + 1) < 0)
 		{
 			LOGE("%s(): Socket Write Failure: %s\n", __func__, strerror(errno));
 			return 1;
 		}
-		
+
 		/* If we're in the same mode, then send start message directly */
 		if (new_mode == usb_current_mode)
 		{
@@ -601,12 +599,11 @@ static int usbd_socket_event(int sockfd)
 				LOGE("%s(): Socket Write Failure: %s\n", __func__, strerror(errno));
 				return 1;
 			}
-			
 		}
 		else
 		{
 			res = usbd_set_usb_mode(new_mode);
-			
+
 			/* If we are handling it on our own, then it doesn't reenumerate so send the start message right away */
 			if (res == 1)
 			{
@@ -616,9 +613,7 @@ static int usbd_socket_event(int sockfd)
 					return 1;
 				}
 			}
-				
 		}
-		
 		return 0;
 	}
 	else
@@ -637,18 +632,18 @@ static int process_usb_uevent_message()
 	char* power_supply_model_name = NULL;
 	char* ptr;
 	char* end;
-	
+
 	int res = recv(uevent_fd, buffer, ARRAY_SIZE(buffer), 0);
-	
+
 	if (res <= 0)
 		return 0;
-	
+
 	ptr = buffer;
 	end = &(buffer[res]);
-	
+
 	if (ptr > end)
-		return 0;	
-	
+		return 0;
+
 	do
 	{
 		if (!strncmp(POWER_SUPPLY_TYPE_EVENT, ptr, strlen(POWER_SUPPLY_TYPE_EVENT)))
@@ -661,13 +656,13 @@ static int process_usb_uevent_message()
 		ptr += strlen(ptr) + 1;
 		
 	} while (end > ptr);
-	
+
 	/* Now check what we got */
-	
+
 	/* Power supply type: can be NULL, Battery or USB */
 	if (!power_supply_type || strcmp(power_supply_type, "USB"))
 		return 1;
-	
+
 	/* We got cable, now check if it's factory or not */
 	if (power_supply_model_name)
 	{
@@ -680,7 +675,7 @@ static int process_usb_uevent_message()
 	}
 	else
 		LOGI("%s(): cable type not specified in USB Event\n", __func__);
-	
+
 	/* Check if we have power supply */
 	if (power_supply_online)
 	{
@@ -717,18 +712,18 @@ static int process_usb_uevent_message()
 static int usb_req_mode_switch(const char* new_mode)
 {
 	int new_mode_index;
-	
+
 	new_mode_index = usbd_get_mode_index(new_mode, USBMOD_REQ_SWITCH);
-	
+
 	if (new_mode_index < 0)
 		return 1;
-	
+
 	LOGI("%s(): switch_req=%s\n", __func__, new_mode);
-	
+
 	if (usbd_app_fd >= 0)
 	{
 		LOGI("%s(): usb switch to %s\n", __func__, new_mode);
-		
+
 		if (write(usbd_app_fd, usb_modes[new_mode_index].req_switch, strlen(usb_modes[new_mode_index].req_switch) + 1) < 0)
 		{
 			LOGE("%s(): Socket Write Failure: %s\n", __func__, strerror(errno));
@@ -737,7 +732,7 @@ static int usb_req_mode_switch(const char* new_mode)
 			return 1;
 		}
 	}
-	
+
 	return 0;
 }
 
@@ -754,14 +749,14 @@ int main(int argc, char **argv)
 	int addr_len;
 	int sockfd;
 	fd_set socks;
-	
+
 	LOGI("%s(): Start usbd - version " USBD_VER "\n", __func__);
-	
+
 	/* init uevent */
 	LOGI("%s(): Initializing uevent_socket \n", __func__);
 	if (open_uevent_socket())
 		return 1;
-	
+
 	/* open device mode */
 	LOGI("%s(): Initializing usb_device_mode \n", __func__);
 	usb_device_fd = open("/dev/usb_device_mode", O_RDWR);
@@ -771,7 +766,7 @@ int main(int argc, char **argv)
 		LOGE("%s(): Unable to open usb_device_mode '%s'\n", __func__, strerror(errno));
 		return 1;
 	}
-	
+
 	/* init usdb socket */
 	LOGI("%s(): Initializing usbd socket \n", __func__);
 	if (init_usdb_socket())
@@ -779,37 +774,37 @@ int main(int argc, char **argv)
 		LOGE("%s(): failed to create socket server '%s'\n", __func__, strerror(errno));
 		return 1;
 	}
-	
+
 	/* init cable status */
 	if (usbd_get_cable_status() < 0)
 	{
 		LOGE("%s(): failed to get cable status\n", __func__);
 		return 1;
 	}
-	
+
 	if (usb_online)
 		cable_msg = "Cable Attached";
 	else
 		cable_msg = "Cable Detached";
-	
+
 	LOGI("%s(): Initial Cable State = %s\n", __func__, cable_msg);
-	
+
 	while (1)
 	{
 		/* wait for sockets */
 		FD_ZERO(&socks);
-		
+
 		FD_SET(uevent_fd, &socks);
 		FD_SET(usb_device_fd, &socks);
 		FD_SET(usbd_socket_fd, &socks);
-		
+
 		if (usbd_app_fd >= 0)
 			FD_SET(usbd_app_fd, &socks);
-		
+
 		select(get_highest_socket_fd() + 1, &socks, NULL, NULL, NULL);
-		
+
 		/* Check received data */
-		
+
 		/* uevent socket */
 		if (FD_ISSET(uevent_fd, &socks) && !process_usb_uevent_message())
 		{
@@ -831,11 +826,10 @@ int main(int argc, char **argv)
 						close(usbd_app_fd);
 						usbd_app_fd = -1;
 					}
-					
+
 					/* Set to no mode if we're disconnected */
 					if (usb_state == USBDSTAT_CABLE_DISCONNECTED)
 						usbd_set_usb_mode(0);
-					
 				}
 			}
 		}
@@ -850,27 +844,27 @@ int main(int argc, char **argv)
 			{
 				LOGI("%s(): devbuf: %s\n"
 				     "rc: %d usbd_state: %d\n", __func__, buffer, strlen(buffer), usb_state);
-				
+
 				/* PC switch buffer */
 				pch = strtok(buffer, ":");
 				
 				if (pch != NULL)
 					strcpy(pc_switch_buf, pch);
 				else
-					memset(pc_switch_buf, 0, sizeof(pc_switch_buf)); 
+					memset(pc_switch_buf, 0, sizeof(pc_switch_buf));
 				
 				LOGI("%s(): pc_switch_buf = %s\n", __func__, pc_switch_buf);
-				
+
 				/* ADB enable buffer */
 				pch = strtok(NULL, ":");
 				
 				if (pch != NULL)
 					strcpy(adb_enable_buf, pch);
 				else
-					memset(adb_enable_buf, 0, sizeof(adb_enable_buf)); 
+					memset(adb_enable_buf, 0, sizeof(adb_enable_buf));
 				
 				LOGI("%s(): adb_enable_buf = %s\n", __func__, adb_enable_buf);
-								
+
 				/* Enumeration */
 				pch = strtok(NULL, ":");
 				
@@ -881,7 +875,7 @@ int main(int argc, char **argv)
 				}
 				else
 					memset(enum_buf, 0, sizeof(enum_buf));
-				
+
 				if (usbd_app_fd >= 0)
 				{
 					/* Evaluate adb status */
@@ -890,10 +884,10 @@ int main(int argc, char **argv)
 					else if (!strcmp(adb_enable_buf, USBD_DEV_EVENT_ADB_DISABLE))
 						usbd_send_adb_status(usbd_app_fd, 0);
 				}
-				
+
 				if (pc_switch_buf[0] != '\0' && strcmp(pc_switch_buf, "none"))
 					usb_req_mode_switch(pc_switch_buf);
-				
+
 				if (!strncmp(enum_buf, USBD_DEV_EVENT_GET_DESCRIPTOR, strlen(USBD_DEV_EVENT_GET_DESCRIPTOR)))
 				{
 					/* Make sure it is not spurious */
@@ -920,22 +914,21 @@ int main(int argc, char **argv)
 					LOGI("%s(): received enumerated\n", __func__);
 					LOGI("%s(): usbd_app_fd  = %d\n", __func__, usbd_app_fd);
 					usb_state = USBDSTAT_USB_ENUMERATED;
-					
+
 					if (usbd_app_fd >= 0 && usbd_enum_process(usbd_app_fd))
 					{
 						close(usbd_app_fd);
 						usbd_app_fd = -1;
 					}
-					
 				}
 			}
 		}
-		
+
 		/* socket */
 		if (FD_ISSET(usbd_socket_fd, &socks))
 		{
 			LOGI("%s(): get event from usbd server fd\n", __func__);
-			
+
 			sockfd = accept(usbd_socket_fd, &addr, &addr_len);
 			if (sockfd >= 0)
 			{
@@ -947,18 +940,18 @@ int main(int argc, char **argv)
 				else
 				{
 					LOGI("%s(): Estabilished socket connection with the App\n", __func__);
-					
+
 					usbd_app_fd = sockfd;
 					usbd_send_adb_status(usbd_app_fd, get_adb_enabled_status());
 					usbd_notify_current_status(usbd_app_fd);
 				}
 			}
 		}
-		
+
 		if (usbd_app_fd >= 0 && FD_ISSET(usbd_app_fd, &socks))
 		{
 			LOGI("%s(): Read and handle a pending message from the App\n", __func__);
-			
+
 			if (usbd_socket_event(usbd_app_fd))
 			{
 				close(usbd_app_fd);
@@ -966,4 +959,4 @@ int main(int argc, char **argv)
 			}
 		}
 	}
-} 
+}
