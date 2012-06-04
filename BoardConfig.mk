c8094b849f3891cd568ffa6d7b50434a0d0d5314
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-06-04 18:37:29 +0200

    BOARD_WITHOUT_IPV6_QUOTA to prevent ip6tables netd errors
    and restore the hostapd service (see my changes in system/netd)
    
    Change-Id: I1df47a664f0ac44793336e1064ffcc87cae37479
    
    Conflicts:
    
    	BoardConfig.mk
    	profiles/2nd-init/init.mapphone_umts.rc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,12 +60,18 @@ WIFI_DRIVER_FW_AP_PATH      := "/system/etc/wifi/fw_tiwlan_ap.bin"
 WPA_SUPPL_APPROX_USE_RSSI   := true
 WPA_SUPPL_WITH_SIGNAL_POLL  := true
 # CM9
+WIFI_DRIVER_LOADER_DELAY    := 200000
 WIFI_AP_DRIVER_MODULE_PATH  := "/system/lib/modules/tiap_drv.ko"
 WIFI_AP_DRIVER_MODULE_NAME  := tiap_drv
 WIFI_AP_FIRMWARE_LOADER     := wlan_ap_loader
 WIFI_AP_DRIVER_MODULE_ARG   := ""
+BOARD_HOSTAPD_SERVICE_NAME  := hostapd
+BOARD_HOSTAPD_NO_ENTROPY    := true
 BOARD_HOSTAPD_DRIVER        := true
 BOARD_HOSTAPD_DRIVER_NAME   := wilink
+
+# Netfilter
+BOARD_WITHOUT_IPV6_QUOTA := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
