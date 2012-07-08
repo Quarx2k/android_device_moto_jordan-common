b2a3d1e838de18f2dc32c7a853e0005e80a901cf
Quarx2k <agent00791@gmail.com>
2012-07-08 18:10:25 +0700

    one module for defy/defy+

--- bootmenu/script/pvr.sh
+++ bootmenu/script/pvr.sh
@@ -2,20 +2,22 @@
 #
 # Load PVR Driver.
 
-PATH=/system/xbin:$PATH
+export PATH=/sbin:/system/xbin:/system/bin
 
-MODULE="/system/lib/modules/pvroff.ko"
+MAJOR=252
+HOOK=0
 
 if [ -f /proc/socinfo ]; then
-MODULE="/system/lib/modules/pvroff_plus.ko"
+MAJOR=246
+HOOK=1
 fi
 
-  insmod /system/lib/modules/symsearch.ko
-  insmod $MODULE
+  /system/xbin/insmod /system/lib/modules/symsearch.ko
+  /system/xbin/insmod /system/lib/modules/pvroff.ko major_number=$MAJOR hook_enable=$HOOK
   echo 0 > /sys/devices/platform/omapdss/overlay0/enabled 
   echo 4194304 > /sys/devices/platform/omapfb/graphics/fb0/size
   echo 1 > /sys/devices/platform/omapdss/overlay0/enabled
-  insmod /system/lib/modules/pvrsrvkm.ko
-  insmod /system/lib/modules/omaplfb.ko
+  /system/xbin/insmod /system/lib/modules/pvrsrvkm.ko
+  /system/xbin/insmod /system/lib/modules/omaplfb.ko
 
 
