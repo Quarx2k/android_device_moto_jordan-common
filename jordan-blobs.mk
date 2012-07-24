254020c8180b942155abb30b8e741e341531819b
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-07-24 10:10:25 +0200

    move etc and usr folders, its not prebuilt
    Change-Id: I7ffec394a63829e4466ab6ff106a22e7f88a26a3
    
    Conflicts:
    
    	jordan-blobs.mk

--- jordan-blobs.mk
+++ jordan-blobs.mk
@@ -14,57 +14,51 @@
 
 device_path = device/moto/jordan-common
 
-DEVICE_PREBUILT := ${device_path}/prebuilt
-
 PRODUCT_COPY_FILES += \
-	$(DEVICE_PREBUILT)/etc/terminfo/l/linux:system/etc/terminfo/l/linux \
-	$(DEVICE_PREBUILT)/etc/terminfo/x/xterm:system/etc/terminfo/x/xterm \
+	$(device_path)/etc/terminfo/l/linux:system/etc/terminfo/l/linux \
+	$(device_path)/etc/terminfo/x/xterm:system/etc/terminfo/x/xterm \
 
 # Key layouts, names must fit the ones in /proc/bus/input/devices, qwerty.kl is the fallback one.
 PRODUCT_COPY_FILES += \
-	$(DEVICE_PREBUILT)/usr/idc/internal.idc:system/usr/idc/lm3530_led.idc \
-	$(DEVICE_PREBUILT)/usr/idc/internal.idc:system/usr/idc/accelerometer.idc \
-	$(DEVICE_PREBUILT)/usr/idc/internal.idc:system/usr/idc/compass.idc \
-	$(DEVICE_PREBUILT)/usr/idc/internal.idc:system/usr/idc/light-prox.idc \
-	$(DEVICE_PREBUILT)/usr/idc/internal.idc:system/usr/idc/proximity.idc \
-	$(DEVICE_PREBUILT)/usr/idc/sholes-keypad.idc:system/usr/idc/sholes-keypad.idc \
-	$(DEVICE_PREBUILT)/usr/idc/cpcap-key.idc:system/usr/idc/cpcap-key.idc \
-	$(DEVICE_PREBUILT)/usr/idc/qtouch-touchscreen.idc:system/usr/idc/qtouch-touchscreen.idc \
-	$(DEVICE_PREBUILT)/usr/qwerty.kl:system/usr/keylayout/qwerty.kl \
-	$(DEVICE_PREBUILT)/usr/qwerty.kl:system/usr/keylayout/qtouch-touchscreen.kl \
-	$(DEVICE_PREBUILT)/usr/keypad.kl:system/usr/keylayout/sholes-keypad.kl \
-	$(DEVICE_PREBUILT)/usr/keypad.kl:system/usr/keylayout/umts_jordan-keypad.kl \
-	$(DEVICE_PREBUILT)/usr/cpcap-key.kl:system/usr/keylayout/cpcap-key.kl \
-	$(DEVICE_PREBUILT)/usr/keychars/cpcap-key.kcm:system/usr/keychars/cpcap-key.kcm \
+	$(device_path)/usr/idc/internal.idc:system/usr/idc/lm3530_led.idc \
+	$(device_path)/usr/idc/internal.idc:system/usr/idc/accelerometer.idc \
+	$(device_path)/usr/idc/internal.idc:system/usr/idc/compass.idc \
+	$(device_path)/usr/idc/internal.idc:system/usr/idc/light-prox.idc \
+	$(device_path)/usr/idc/internal.idc:system/usr/idc/proximity.idc \
+	$(device_path)/usr/idc/sholes-keypad.idc:system/usr/idc/sholes-keypad.idc \
+	$(device_path)/usr/idc/cpcap-key.idc:system/usr/idc/cpcap-key.idc \
+	$(device_path)/usr/idc/qtouch-touchscreen.idc:system/usr/idc/qtouch-touchscreen.idc \
+	$(device_path)/usr/qwerty.kl:system/usr/keylayout/qwerty.kl \
+	$(device_path)/usr/qwerty.kl:system/usr/keylayout/qtouch-touchscreen.kl \
+	$(device_path)/usr/keypad.kl:system/usr/keylayout/sholes-keypad.kl \
+	$(device_path)/usr/keypad.kl:system/usr/keylayout/umts_jordan-keypad.kl \
+	$(device_path)/usr/cpcap-key.kl:system/usr/keylayout/cpcap-key.kl \
+	$(device_path)/usr/keychars/cpcap-key.kcm:system/usr/keychars/cpcap-key.kcm \
 
 PRODUCT_COPY_FILES += \
 	${device_path}/vold.fstab:system/etc/vold.fstab \
 	${device_path}/media_profiles.xml:system/etc/media_profiles.xml \
 	${device_path}/modules/modules.alias:system/lib/modules/modules.alias \
 	${device_path}/modules/modules.dep:system/lib/modules/modules.dep \
-	$(DEVICE_PREBUILT)/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
-	$(DEVICE_PREBUILT)/etc/init.d/02baseband:system/etc/init.d/02baseband \
-	$(DEVICE_PREBUILT)/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
-	$(DEVICE_PREBUILT)/etc/init.d/04filesystems:system/etc/init.d/04filesystems \
-	$(DEVICE_PREBUILT)/etc/init.d/05mountsd:system/etc/init.d/05mountsd \
-	$(DEVICE_PREBUILT)/etc/init.d/08backlight:system/etc/init.d/08backlight \
-	$(DEVICE_PREBUILT)/etc/init.d/10gpiofix:system/etc/init.d/10gpiofix \
-	$(DEVICE_PREBUILT)/etc/init.d/90multitouch:system/etc/init.d/90multitouch \
-	$(DEVICE_PREBUILT)/etc/profile:system/etc/profile \
-	$(DEVICE_PREBUILT)/etc/sysctl.conf:system/etc/sysctl.conf \
-	$(DEVICE_PREBUILT)/etc/busybox.fstab:system/etc/fstab \
-	$(DEVICE_PREBUILT)/etc/wifi/dnsmasq.conf:system/etc/wifi/dnsmasq.conf \
-	$(DEVICE_PREBUILT)/etc/wifi/tiwlan.ini:system/etc/wifi/tiwlan.ini \
-	$(DEVICE_PREBUILT)/etc/wifi/tiwlan_ap.ini:system/etc/wifi/tiwlan_ap.ini \
-	$(DEVICE_PREBUILT)/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-	$(DEVICE_PREBUILT)/etc/gpsconfig.xml:system/etc/gpsconfig.xml \
-	$(DEVICE_PREBUILT)/etc/location.cfg:system/etc/location.cfg \
-	$(DEVICE_PREBUILT)/etc/media_codecs.xml:system/etc/media_codecs.xml \
-	$(DEVICE_PREBUILT)/etc/audio_policy.conf:system/etc/audio_policy.conf \
-	device/moto/jordan-common/vold.fstab:system/etc/vold.fstab \
-	device/moto/jordan-common/media_profiles.xml:system/etc/media_profiles.xml \
-	device/moto/jordan-common/modules/modules.alias:system/lib/modules/modules.alias \
-	device/moto/jordan-common/modules/modules.dep:system/lib/modules/modules.dep \
+	$(device_path)/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
+	$(device_path)/etc/init.d/02baseband:system/etc/init.d/02baseband \
+	$(device_path)/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
+	$(device_path)/etc/init.d/04filesystems:system/etc/init.d/04filesystems \
+	$(device_path)/etc/init.d/05mountsd:system/etc/init.d/05mountsd \
+	$(device_path)/etc/init.d/08backlight:system/etc/init.d/08backlight \
+	$(device_path)/etc/init.d/10gpiofix:system/etc/init.d/10gpiofix \
+	$(device_path)/etc/init.d/90multitouch:system/etc/init.d/90multitouch \
+	$(device_path)/etc/profile:system/etc/profile \
+	$(device_path)/etc/sysctl.conf:system/etc/sysctl.conf \
+	$(device_path)/etc/busybox.fstab:system/etc/fstab \
+	$(device_path)/etc/wifi/dnsmasq.conf:system/etc/wifi/dnsmasq.conf \
+	$(device_path)/etc/wifi/tiwlan.ini:system/etc/wifi/tiwlan.ini \
+	$(device_path)/etc/wifi/tiwlan_ap.ini:system/etc/wifi/tiwlan_ap.ini \
+	$(device_path)/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+	$(device_path)/etc/gpsconfig.xml:system/etc/gpsconfig.xml \
+	$(device_path)/etc/location.cfg:system/etc/location.cfg \
+	$(device_path)/etc/media_codecs.xml:system/etc/media_codecs.xml \
+	$(device_path)/etc/audio_policy.conf:system/etc/audio_policy.conf \
 
 # New CM9 backup list system (addon.d)
 PRODUCT_COPY_FILES += \
