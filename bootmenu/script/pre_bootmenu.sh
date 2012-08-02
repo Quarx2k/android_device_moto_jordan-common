daa24c69ebd6e7f80895e121e5d3b5076071047d
Quarx2k <agent00791@gmail.com>
2012-08-02 15:20:14 +0700

    early start of TLS insmod

--- bootmenu/script/pre_bootmenu.sh
+++ bootmenu/script/pre_bootmenu.sh
@@ -3,6 +3,10 @@
 ######## BootMenu Script
 ######## Execute Pre BootMenu
 
+#We should insmod TLS Module before start bootmenu
+/system/bootmenu/binary/busybox insmod /system/lib/modules/symsearch.ko
+/system/bootmenu/binary/busybox insmod /system/lib/modules/tls-enable.ko
+
 source /system/bootmenu/script/_config.sh
 
 ######## Main Script
@@ -77,11 +81,6 @@ if [ ! -d /cache/recovery ]; then
 fi
 
 mkdir -p /cache/bootmenu
-
-insmod /system/lib/modules/symsearch.ko
-
-#insmod TLS Register module
-insmod /system/lib/modules/tls-enable.ko
 
 # load ondemand safe settings to reduce heat and battery use
 if [ -x /system/bootmenu/script/overclock.sh ]; then
