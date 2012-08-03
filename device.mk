f22e62eaeb99cc52e4bb50245904b8c8fed4a62d
Quarx2k <agent00791@gmail.com>
2012-08-03 22:02:10 +0700

    add Romupdater

--- device.mk
+++ device.mk
@@ -100,7 +100,7 @@ PRODUCT_PACKAGES += librs_jni
 PRODUCT_PACKAGES += DroidSSHd dropbear dropbearkey sftp-server scp ssh rsync
 
 # CM9 apps
-PRODUCT_PACKAGES += FileManager Torch HwaSettings make_ext4fs
+PRODUCT_PACKAGES += FileManager Torch HwaSettings make_ext4fs RomUpdater
 
 # Experimental TI OpenLink
 PRODUCT_PACKAGES += libnl_2 iw
