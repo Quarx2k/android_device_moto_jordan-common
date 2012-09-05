f2c8b57be3d525204f793bc6b0ba5e14b9fc2e44
Quarx2k <agent00791@gmail.com>
2012-09-05 14:52:38 +0700

    cleanup qtouch unload module

--- modules/qtouch_obp_ts/qtouch_unload.c
+++ modules/qtouch_obp_ts/qtouch_unload.c
@@ -11,46 +11,20 @@
 
 
 #include <linux/module.h>
-#include <linux/moduleparam.h>
 #include <linux/smp_lock.h>
 #include <linux/i2c.h>
-#include <linux/platform_device.h>
-#include <linux/device.h>
-#include <linux/fs.h>
-#include <linux/io.h>
-#include <linux/pci.h>
-#include <plat/display.h>
-#include <plat/vrfb.h>
-#include "../symsearch/symsearch.h"
 
 #define TAG "qtouch_unload"
 
 struct driver_private {
 	struct kobject kobj;
-	struct klist klist_devices;
-	struct klist_node knode_bus;
-	struct module_kobject *mkobj;
-	struct device_driver *driver;
 };
-
-static struct i2c_driver* qtouch_ts_driver;
-struct workqueue_struct *qtouch_ts_wq;
-
-SYMSEARCH_DECLARE_ADDRESS_STATIC(qtouch_ts_driver);
-SYMSEARCH_DECLARE_ADDRESS_STATIC(qtouch_ts_wq);
 
 static int __init qtouch_unload_init(void)
 {
     struct device_driver *drv;
     struct kobject *kobj;
-    
-    SYMSEARCH_BIND_ADDRESS(qtouch_unload, qtouch_ts_driver);
-    SYMSEARCH_BIND_ADDRESS(qtouch_unload, qtouch_ts_wq);
-    
-    qtouch_ts_driver = (void *)SYMSEARCH_GET_ADDRESS(qtouch_ts_driver);
-    qtouch_ts_wq = (void *)SYMSEARCH_GET_ADDRESS(qtouch_ts_wq);
-    
-
+  
     printk(KERN_INFO "qtouch_unload init\n");
     lock_kernel();
     
@@ -64,7 +38,6 @@ static int __init qtouch_unload_init(void)
 	    driver_unregister(drv);
 	    kobject_del(kobj);
     }
-    
     unlock_kernel();
     return 0;
 }
