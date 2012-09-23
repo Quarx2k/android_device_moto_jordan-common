b7f76b07cb0e6ce97fa6d90e9a4070f377388259
Quarx2k <agent00791@gmail.com>
2012-09-23 17:55:01 +0700

    disable prelink of libcamera

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -8,6 +8,7 @@ LOCAL_SHARED_LIBRARIES:= libdl libutils libcutils libcamera_client
 
 LOCAL_MODULE := libcamera
 LOCAL_MODULE_TAGS := optional
+LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
 
