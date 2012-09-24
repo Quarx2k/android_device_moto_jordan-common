6a7b37c34a39d9f0925922d35db55bb4f8132ba8
Quarx2k <agent00791@gmail.com>
2012-09-24 13:07:24 +0700

    Revert "disable prelink of libcamera"
    This reverts commit b7f76b07cb0e6ce97fa6d90e9a4070f377388259.

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -8,7 +8,6 @@ LOCAL_SHARED_LIBRARIES:= libdl libutils libcutils libcamera_client
 
 LOCAL_MODULE := libcamera
 LOCAL_MODULE_TAGS := optional
-LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
 
