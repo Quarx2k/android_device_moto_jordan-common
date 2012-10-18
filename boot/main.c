abd979779ce225fab81220dcbb66cc0ece28baf7
Quarx2k <agent00791@gmail.com>
2012-10-18 13:03:19 +0700

    enable l2 cache (thanks to SkrilaxCZ)

--- boot/main.c
+++ boot/main.c
@@ -19,6 +19,19 @@ void critical_error(error_t err)
 	while (1);
 }
 
+void __attribute__((__naked__)) l2cache_enable(void)
+{
+	/* We have ES3.1 OMAP3430 on the phones at least, so we can set it directly */
+	__asm__ volatile 
+	(
+		"stmfd sp!, {r4,lr}\n"
+		"mrc p15, 0, r4, c1, c0, 1\n"
+		"orr r4, r4, #0x2\n"
+		"mcr p15, 0, r4, c1, c0, 1\n"
+		"ldmfd sp!, {r4,pc}\n"
+	);
+}
+
 void __attribute__((__naked__)) enter_kernel(int zero, int arch, void *atags, int kern_addr) 
 {
 	__asm__ volatile (
@@ -51,6 +64,7 @@ int main()
 		printf("Watchdog disabled.\n");
 		
 		printf("BOOTING KERNEL!\n");
+		l2cache_enable();
 		enter_kernel(0, ARCH_NUMBER, atags, KERNEL_DEST);
 	}
 	else 
