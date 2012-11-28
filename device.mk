d0c7b45f0851625ab40553676ccf62af3fd71342
Quarx2k <agent00791@gmail.com>
2012-11-28 19:25:18 +0700

    cleanup

--- device.mk
+++ device.mk
@@ -84,7 +84,7 @@ PRODUCT_PACKAGES += tiap_loader tiap_cu ndc hostapd.conf
 PRODUCT_PACKAGES += dspexec libbridge libLCML libOMX_Core
 PRODUCT_PACKAGES += libOMX.TI.AAC.encode libOMX.TI.AAC.decode libOMX.TI.AMR.decode libOMX.TI.AMR.encode
 PRODUCT_PACKAGES += libOMX.TI.WBAMR.encode libOMX.TI.MP3.decode libOMX.TI.WBAMR.decode
-PRODUCT_PACKAGES += libOMX.TI.WMA.decode libOMX.TI.Video.Decoder libOMX.TI.Video.encoder
+PRODUCT_PACKAGES += libOMX.TI.Video.Decoder libOMX.TI.Video.encoder
 PRODUCT_PACKAGES += libOMX.TI.JPEG.Encoder #libskiahw libOMX.TI.JPEG.decoder
 
 # Defy stuff
