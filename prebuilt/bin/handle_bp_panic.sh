791825be2c8e7e3dfe680fecd29ae89aeefe8dc1
Quarx2k <agent00791@gmail.com>
2012-12-29 17:05:23 +0100

    BP panic handling.
    - Actually make panic_daemon start and handle panics correctly
    - Instead of a plain reboot, show UI for the failure. Currently, this is
      limited to a notification that triggers a reboot when touched, but
      that will be improved later.

---
+++ prebuilt/bin/handle_bp_panic.sh
@@ -0,0 +1,11 @@
+#!/system/bin/sh
+
+# stop panic_daemon restarting
+setprop ctl.stop panic_daemon
+
+# let UI handle the failure
+am startservice -a com.cyanogenmod.settings.device.action.HANDLE_BP_PANIC -n com.cyanogenmod.settings.device/.BpPanicHandlerService 2>&1 | grep Error
+if [ $? -eq 0 ]; then
+    # some error occured, use fallback and reboot directly
+    reboot bppanic
+fi
