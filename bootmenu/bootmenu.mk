7203f7b736789e20f2777f58bb92c516ca7829c6
Quarx2k <agent00791@gmail.com>
2013-01-23 14:00:16 +0700

    more updates for 3.0

--- bootmenu/bootmenu.mk
+++ bootmenu/bootmenu.mk
@@ -28,7 +28,6 @@ PRODUCT_COPY_FILES += \
 # Ramdisk
 PRODUCT_COPY_FILES += \
 	${bm_device}/profiles/ramdisk/ueventd.mapphone_umts.rc:root/ueventd.mapphone_umts.rc \
-	${bm_device}/profiles/ramdisk/init.usb.rc:root/init.usb.rc \
 	${bm_device}/profiles/ramdisk/init.mapphone_umts.rc:root/init.mapphone_umts.rc \
 
 # scripts
