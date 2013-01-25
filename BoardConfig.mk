ae8c13e0fd46afc7eb4058f739f549f0c62c63e7
Quarx2k <agent00791@gmail.com>
2013-01-25 15:40:54 +0700

    remove custom toolchain for kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,7 +169,6 @@ hboot:
 
 # If kernel sources are present in repo, here is the location
 TARGET_KERNEL_SOURCE := $(ANDROID_BUILD_TOP)/jordan-kernel
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.5.4-linaro
 TARGET_KERNEL_CONFIG  := mapphone_defconfig
 BOARD_KERNEL_CMDLINE := console=/dev/null mem=498M omapfb.vram=0:4M
 #TARGET_PREBUILT_KERNEL := $(ANDROID_BUILD_TOP)/device/moto/jordan-common/kernel
