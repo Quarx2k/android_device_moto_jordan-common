733d22684a027ced2524a670b7139f7d8170cf9f
Quarx2k <agent00791@gmail.com>
2013-04-21 21:03:40 +0700

    a2dp_set_sink stub

--- libfnc/libfnc.c
+++ libfnc/libfnc.c
@@ -264,6 +264,12 @@ return 0;
 return 0;
 }
 
+ int a2dp_set_sink()
+{
+ ALOGW(__func__);
+return 0;
+}
+
  int a2dp_write()
 {
  ALOGW(__func__);
@@ -287,4 +293,3 @@ return 0;
  ALOGW(__func__);
 return 0;
 }
-
