b5c54c28e4343b3de558da3f76651499ad124658
Quarx2k <agent00791@gmail.com>
2013-06-26 13:37:06 +0700

    disable wifi calibration after first boot

--- prebuilt/bin/wifical.sh
+++ prebuilt/bin/wifical.sh
@@ -41,8 +41,15 @@ rm /system/etc/firmware/ti-connectivity/wl1271-nvs.bin
 # Leaving mac address field empty for random mac
 calibrator plt autocalibrate wlan0 $WL12xx_MODULE $TARGET_INI_FILE $TARGET_NVS_FILE $HW_MAC
 
+# Disable Wifi Calibration after first boot
+if [ -e $TARGET_NVS_FILE ];
+then
+	busybox mv -f /system/bin/wifical.sh /system/bin/wifical.sh_disabled
+fi
+
 # Remount system partition as ro
 mount -o remount,ro /system
+
 
 echo " ******************************"
 echo " * Finished Wi-Fi calibration *"
