5f206c2ad24b24056709a9f64145405a2608830f
Quarx2k <agent00791@gmail.com>
2013-07-08 17:03:27 +0700

    update OC script

--- bootmenu/script/overclock.sh
+++ bootmenu/script/overclock.sh
@@ -14,41 +14,17 @@ ASKED_MODE=$1
 #############################################################
 
 ##### Default Param
-# enable 0
-# load_all 0
-# scaling 2
-# clk1 300
-# clk2 600
-# clk3 1000
-# vsel1 30
-# vsel2 46
-# vsel3 58
-# con_up_threshold 80
-# con_down_threshold 20
-# con_freq_step 5
-# con_sampling_rate 160000
-# int_min_sample_rate 50000
-# ond_up_threshold 86
-# ond_sampling_rate 50000
-# smt_min_cpu_load 20
-# smt_max_cpu_load 80
-# smt_awake_min_freq 0
-# smt_sleep_max_freq 250000
-# smt_up_min_freq 1000000
-# smt_wakeup_freq 1000000
-# smt_ramp_up_step 250000
-# bst_awake_ideal_freq 800000
-# bst_down_rate_us 97000
-# bst_max_cpu_load 70
-# bst_min_cpu_load 40
-# bst_ramp_down_step 160000
-# bst_ramp_up_step 160000
-# bst_sample_rate_jiffies 2
-# bst_sleep_ideal_freq 300000
-# bst_sleep_wakeup_freq 300000
-# bst_up_rate_us 52000
-# bst_debug_mask 0
-# iosched_sio 0
+#enable 1
+#scaling 0
+#sched 1
+#clk1 300
+#clk2 600
+#clk3 800
+#clk4 1000
+#vsel1 33
+#vsel2 48
+#vsel3 58
+#vsel4 62
 
 param_load()
 {
@@ -64,18 +40,16 @@ param_safe()
   # enable ondemand profile
   # which is in kernel
   export enable=1
-  export load_all=0
+  export scaling=0
+  export sched=1
   export clk1=300
   export clk2=600
   export clk3=800
   export clk4=1000
-  export vsel1=30
-  export vsel2=46
-  export vsel3=52
-  export vsel4=58
-  export scaling=2
-  export ond_up_threshold=86
-  export ond_sampling_rate=50000
+  export vsel1=33
+  export vsel2=48
+  export vsel3=58
+  export vsel4=62
   echo 1 > /sys/power/sr_vdd2_autocomp
 }
 
@@ -112,57 +86,22 @@ set_scaling()
 {
   case "$scaling" in
     "0" )
-      echo "conservative" > $SCALING_GOVERNOR
-      echo $con_freq_step > /sys/devices/system/cpu/cpu0/cpufreq/conservative/freq_step
-      echo $con_down_threshold > /sys/devices/system/cpu/cpu0/cpufreq/conservative/down_threshold
-      echo $con_sampling_rate > /sys/devices/system/cpu/cpu0/cpufreq/conservative/sampling_rate
-      echo $con_up_threshold > /sys/devices/system/cpu/cpu0/cpufreq/conservative/up_threshold
-    ;;
+      echo "interactive" > $SCALING_GOVERNOR;;
     "1" )
-      echo "interactive" > $SCALING_GOVERNOR
-    ;;
+      echo "ondemand" > $SCALING_GOVERNOR;;
     "2" )
-      echo "ondemand" > $SCALING_GOVERNOR
-      echo $ond_sampling_rate > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
-      echo $ond_up_threshold > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
-    ;;
+      echo "performance" > $SCALING_GOVERNOR;;
     "3" )
-      echo "performance" > $SCALING_GOVERNOR
-    ;;
+      echo "powersave" > $SCALING_GOVERNOR;;
     "4" )
-      echo "powersave" > $SCALING_GOVERNOR
-    ;;
+      echo "boosted" > $SCALING_GOVERNOR;;
     "5" )
-      echo boosted > $SCALING_GOVERNOR
-      echo $bst_debug_mask     > /sys/devices/system/cpu/cpufreq/boosted/debug_mask
-      echo $bst_awake_ideal_freq > /sys/devices/system/cpu/cpufreq/boosted/awake_ideal_freq
-      echo $bst_down_rate_us   > /sys/devices/system/cpu/cpufreq/boosted/down_rate_us
-      echo $bst_max_cpu_load   > /sys/devices/system/cpu/cpufreq/boosted/max_cpu_load
-      echo $bst_min_cpu_load   > /sys/devices/system/cpu/cpufreq/boosted/min_cpu_load
-      echo $bst_ramp_down_step > /sys/devices/system/cpu/cpufreq/boosted/ramp_down_step
-      echo $bst_ramp_up_step   > /sys/devices/system/cpu/cpufreq/boosted/ramp_up_step
-      echo $bst_sample_rate_jiffies > /sys/devices/system/cpu/cpufreq/boosted/sample_rate_jiffies
-      echo $bst_sleep_ideal_freq    > /sys/devices/system/cpu/cpufreq/boosted/sleep_ideal_freq
-      echo $bst_sleep_wakeup_freq   > /sys/devices/system/cpu/cpufreq/boosted/sleep_wakeup_freq
-      echo $bst_up_rate_us     > /sys/devices/system/cpu/cpufreq/boosted/up_rate_us
-    ;;
-    "6" )
-      echo "smartass" > $SCALING_GOVERNOR
-      echo $smt_min_cpu_load > /sys/devices/system/cpu/cpu0/cpufreq/smartass/min_cpu_load
-      echo $smt_max_cpu_load > /sys/devices/system/cpu/cpu0/cpufreq/smartass/max_cpu_load
-      echo $smt_awake_min_freq > /sys/devices/system/cpu/cpu0/cpufreq/smartass/awake_min_freq
-      echo $smt_sleep_max_freq > /sys/devices/system/cpu/cpu0/cpufreq/smartass/sleep_max_freq
-      echo $smt_up_min_freq > /sys/devices/system/cpu/cpu0/cpufreq/smartass/up_min_freq
-      echo $smt_wakeup_freq > /sys/devices/system/cpu/cpu0/cpufreq/smartass/sleep_wakeup_freq
-      echo $smt_ramp_up_step > /sys/devices/system/cpu/cpu0/cpufreq/smartass/ramp_up_step
-    ;;
-    "7" )
-      echo "userspace" > $SCALING_GOVERNOR
-    ;;
+      echo "smartass" > $SCALING_GOVERNOR;;
      * )
     ;;
   esac
 }
+
 
 #############################################################
 # Alternative I/O Schedulers
@@ -173,32 +112,21 @@ set_scaling()
 
 set_ioscheduler()
 {
-  iosched="cfq"
-  if [ "$iosched_sio" -eq "1" ]; then
-      iosched="sio"
-  fi
-
-  # options: 'noop cfq sio'
-  case "$iosched" in
-    "cfq" )
-    ;;
-    "sio" )
-      for i in /sys/block/mmc*/queue; do
-        [ -f "$i/scheduler" ]                 && echo $iosched > $i/scheduler
-        [ -f "$i/iosched/low_latency" ]       && echo 1 > $i/iosched/low_latency
-        [ -f "$i/iosched/back_seek_penalty" ] && echo 1 > $i/iosched/back_seek_penalty
-        [ -f "$i/iosched/back_seek_max" ]     && echo 1000000000 > $i/iosched/back_seek_max
-        [ -f "$i/iosched/slice_idle" ]        && echo 0 > $i/iosched/slice_idle
-        [ -f "$i/iosched/fifo_batch" ]        && echo 1 > $i/iosched/fifo_batch
-        [ -f "$i/iosched/quantum" ]           && echo 16 > $i/iosched/quantum
-        [ -f "$i/nr_requests" ]               && echo 512 > $i/nr_requests
-        [ -f "$i/rotational" ]                && [ "`cat $i/rotational`" -ne "0" ] && echo 0 > $i/rotational
-        [ -f "$i/iostats" ]                   && [ "`cat $i/iostats`" -ne "0" ]    && echo 0 > $i/iostats
-      done
-    ;;
+  case "$sched" in
+    "0" )
+      export iosched="cfq";;
+    "1" )
+      export iosched="deadline";;
+    "2" )
+      export iosched="sio";;
      * )
     ;;
   esac
+
+  for i in /sys/block/mmc*/queue; do
+      [ -f "$i/scheduler" ]                 && echo $iosched > $i/scheduler
+  done
+
 }
 
 #############################################################
