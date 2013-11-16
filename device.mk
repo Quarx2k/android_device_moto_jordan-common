e2d407fb3a6f734011824493e67642b5eab62eae
Quarx2k <agent00791@gmail.com>
2013-11-17 01:44:33 +0700

    disable low_ram by default

--- device.mk
+++ device.mk
@@ -57,7 +57,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	mobiledata.interfaces=rmnet0 \
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.config.low_ram=true  \
+	ro.config.low_ram=false  \
 	dalvik.vm.jit.codecachesize=0 \
 
 DEVICE_PACKAGE_OVERLAYS += device/moto/jordan-common/overlay
