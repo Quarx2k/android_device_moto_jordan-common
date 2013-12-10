5b21162915623a9b3f390a29233dd1248346ac01
Quarx2k <agent00791@gmail.com>
2013-12-10 15:30:14 +0700

    Do not use global build configs

--- full_jordan.mk
+++ full_jordan.mk
@@ -22,7 +22,6 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/moto/jordan-common/device.mk)
 
 PRODUCT_NAME := full_jordan
