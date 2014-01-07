bbd21082e311961d93b44de71ed1fd5f487d4d77
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2014-01-07 14:25:46 +0100

    Convert the changes from common-ota_from_target_files.py to a device-specific hook for the "core" ota_from_target_files script.
    This removes lots of duplicate code.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,7 +147,7 @@ BOARD_HAL_STATIC_LIBRARIES := libhealthd.omap3
 
 # Release tool
 TARGET_PROVIDES_RELEASETOOLS := true
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/moto/jordan-common/releasetools/common_ota_from_target_files
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := build/tools/releasetools/ota_from_target_files --device_specific device/moto/jordan-common/releasetools/jordan-common_ota_from_target_files.py
 
 ext_modules:
 	make -C $(TARGET_KERNEL_MODULES_EXT) modules KERNEL_DIR=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="arm-eabi-"
---
+++ releasetools/jordan-common_ota_from_target_files.py
@@ -0,0 +1,52 @@
+def FullOTA_InstallBegin(self, *args, **kwargs):
+  self.script.AppendExtra('run_program("/sbin/tune2fs", "-O has_journal /dev/block/mmcblk1p24");')
+  self.script.AppendExtra('run_program("/sbin/tune2fs", "-O has_journal /dev/block/mmcblk1p25");')
+
+def FullOTA_InstallEnd(self, *args, **kwargs):
+  self.script.Print("Wiping cache...")
+  self.script.Mount("/cache")
+  self.script.AppendExtra('delete_recursive("/cache");')
+  self.script.Print("Wiping dalvik-cache...")
+  self.script.Mount("/data")
+  self.script.AppendExtra('delete_recursive("/data/dalvik-cache");')
+
+# Bootmenu
+  self.script.SetPermissionsRecursive("/system/etc/init.d", 0, 0, 0755, 0555, None, None)
+  self.script.SetPermissionsRecursive("/system/addon.d", 0, 0, 0755, 0755, None, None)
+  self.script.SetPermissionsRecursive("/system/bootmenu/config", 0, 0, 0755, 0664, None, None)
+  self.script.SetPermissionsRecursive("/system/bootmenu/binary", 0, 0, 0755, 0755, None, None)
+  self.script.SetPermissionsRecursive("/system/bootmenu/script", 0, 0, 0755, 0755, None, None)
+  self.script.SetPermissionsRecursive("/system/bootmenu/recovery/sbin", 0, 0, 0755, 0755, None, None)
+  self.script.SetPermissions("/system/etc/motorola/comm_drv/commdrv_fs.sh", 0, 0, 0755, None, None)
+  self.script.UnpackPackageDir("system/bin/bootmenu", "/system/bootmenu/binary/bootmenu")
+
+  symlinks = []
+
+  symlinks.append(("/system/bin/bootmenu", "/system/bin/logwrapper"))
+  symlinks.append(("indeterminate.png", "/system/bootmenu/images/indeterminate1.png"))
+  symlinks.append(("indeterminate.png", "/system/bootmenu/images/indeterminate2.png"))
+  symlinks.append(("indeterminate.png", "/system/bootmenu/images/indeterminate3.png"))
+  symlinks.append(("indeterminate.png", "/system/bootmenu/images/indeterminate4.png"))
+  symlinks.append(("indeterminate.png", "/system/bootmenu/images/indeterminate5.png"))
+  symlinks.append(("indeterminate.png", "/system/bootmenu/images/indeterminate6.png"))
+
+  # libaudio link fix
+  symlinks.append(("/system/lib/hw/audio.a2dp.default.so", "/system/lib/liba2dp.so"))
+
+  self.script.MakeSymlinks(symlinks)
+  self.script.ShowProgress(0.2, 0)
+
+  self.script.Print("Finished installing Jelly Bean for OMAP3 devices, Enjoy!")
+
+def FullOTA_DisableBootImageInstallation(self, *args, **kwargs):
+  return True
+
+def FullOTA_FormatSystemPartition(self, *args, **kwargs):
+  self.script.Mount("/system")
+  self.script.AppendExtra('delete_recursive("/system");')
+
+  # returning true skips formatting /system!
+  return True
+
+def IncrementalOTA_DisableRecoveryUpdate(self, *args, **kwargs):
+  return True
