7a3682a8a1e0af0c5dd4772f91aa5164fc123864
Quarx2k <agent00791@gmail.com>
2014-01-17 17:31:58 +0700

    add iw_IL language

--- device.mk
+++ device.mk
@@ -174,7 +174,7 @@ PRODUCT_COPY_FILES += \
     $(OUT)/kernel:system/bootmenu/2nd-boot/zImage
 
 # Should be after the full_base include, which loads languages_full
-PRODUCT_LOCALES := en_US en_GB en_IN fr_FR it_IT de_DE es_ES hu_HU uk_UA zh_CN zh_TW ru_RU nl_NL se_SV cs_CZ pl_PL pt_BR da_DK ko_KR el_GR ro_RO
+PRODUCT_LOCALES := en_US en_GB en_IN fr_FR it_IT de_DE es_ES hu_HU uk_UA zh_CN zh_TW ru_RU nl_NL se_SV cs_CZ pl_PL pt_BR da_DK ko_KR el_GR ro_RO iw_IL
 
 # Include drawables for all densities
 PRODUCT_AAPT_CONFIG := hdpi
