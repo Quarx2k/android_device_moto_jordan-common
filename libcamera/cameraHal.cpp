5059435f0a362429f05593e09214fbf5c03b87b6
Quarx2k <agent00791@gmail.com>
2014-01-28 19:43:22 +0700

    fix camera colors

--- libcamera/cameraHal.cpp
+++ libcamera/cameraHal.cpp
@@ -233,8 +233,8 @@ static void Yuv422iToYV12 (unsigned char* dest, unsigned char* src, int width, i
            looks good enough and the speed-up of the conversion
            is significant enough to justify such simplification.
         */
-            *udest++ = src[1];
-            *vdest++ = src[3];
+            *udest++ = src[3];
+            *vdest++ = src[1];
             src += 4;
         }
         src += doubleWidth;
