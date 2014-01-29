0e37402079d0173b97732c4e098e8efa21bd082d
Quarx2k <agent00791@gmail.com>
2014-01-29 17:26:19 +0700

    use 3.0 kernel for twrp

--- bootmenu/bootmenu.mk
+++ bootmenu/bootmenu.mk
@@ -74,5 +74,6 @@ PRODUCT_COPY_FILES += \
 	$(bm_device)/temp/busybox:system/bootmenu/binary/busybox \
 	$(bm_device)/temp/bootmenu:system/bin/bootmenu \
 	$(bm_device)/twrp.fstab:recovery/root/etc/twrp.fstab \
-	$(OUT)/ramdisk-recovery.img:system/bootmenu/2nd-boot/ramdisk-recovery
+	$(OUT)/ramdisk-recovery.img:system/bootmenu/2nd-boot/ramdisk-recovery \
+	${bm_device}/profiles/2nd-boot/zImage-recovery:system/bootmenu/2nd-boot/zImage-recovery \
 
