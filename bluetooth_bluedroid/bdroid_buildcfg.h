33e9de1c4d07d2d234d3a26dbf6dd897bbd9ee04
Quarx2k <agent00791@gmail.com>
2014-03-03 19:07:49 +0700

    disable GATT profile in BT app

--- bluetooth_bluedroid/bdroid_buildcfg.h
+++ bluetooth_bluedroid/bdroid_buildcfg.h
@@ -19,4 +19,7 @@
 
 #define BTM_DEF_LOCAL_NAME   "Motorola Defy"
 #define BTHC_USERIAL_READ_MEM_SIZE (1034)
+#define BLE_INCLUDED FALSE
+#define BTA_GATT_INCLUDED FALSE
+#define SMP_INCLUDED FALSE
 #endif
