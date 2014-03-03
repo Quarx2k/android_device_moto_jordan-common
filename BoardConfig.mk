ea230cdf98204b8c3da0026437748445aa5111d8
Philip <noemail>
2014-03-03 02:22:32 +0000

    don't use squisher
    Change-Id: I0f97d50bee132544a4a18318a0b9e8991371db5b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,7 +165,7 @@ BOARD_HAL_STATIC_LIBRARIES := libhealthd.omap3
 # Release tool
 TARGET_PROVIDES_RELEASETOOLS := true
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := build/tools/releasetools/ota_from_target_files --device_specific device/moto/jordan-common/releasetools/jordan-common_ota_from_target_files.py
-TARGET_SYSTEMIMAGE_USE_SQUISHER := true
+TARGET_SYSTEMIMAGE_USE_SQUISHER := false
 
 ext_modules:
 	make -C $(TARGET_KERNEL_MODULES_EXT) modules KERNEL_DIR=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="arm-eabi-"
