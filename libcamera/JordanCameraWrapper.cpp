4106d0da91bf605016d60f6dfbbd5719e2e048a8
Quarx2k <agent00791@gmail.com>
2014-04-16 19:36:10 +0700

    reenable 720p for bayer, but broke barcode scanner

--- libcamera/JordanCameraWrapper.cpp
+++ libcamera/JordanCameraWrapper.cpp
@@ -506,10 +506,11 @@ JordanCameraWrapper::getParameters() const
 	
     if (deviceIsKobe) {
         ret.set(CameraParameters::KEY_SUPPORTED_PICTURE_SIZES, "320x240,640x480,1280x960,1600x1200,2048x1536");
-    } else {
+    } else if (mCameraType == CAM_SOC) {
     	ret.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES, "176x144,320x240,352x288,640x480,848x480");
+    } else {
+    	ret.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES, "176x144,320x240,352x288,640x480,848x480,1280x720");
     }
-
     ret.set(CameraParameters::KEY_SUPPORTED_VIDEO_SIZES, "");
 
     ret.set("cam-mode", mVideoMode ? "1" : "0");
