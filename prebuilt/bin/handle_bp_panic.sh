47d75df1b99528328ca84facba79fe8e0f16cbd0
Quarx2k <agent00791@gmail.com>
2014-04-30 17:54:04 +0700

    Reboot in android after bp-panic

--- prebuilt/bin/handle_bp_panic.sh
+++ prebuilt/bin/handle_bp_panic.sh
@@ -5,6 +5,8 @@ setprop ctl.stop panic_daemon
 
 # let UI handle the failure
 am startservice -a com.cyanogenmod.settings.device.action.HANDLE_BP_PANIC -n com.cyanogenmod.settings.device/.BpPanicHandlerService 2>&1 | grep Error
+touch /cache/bp_panic
+
 if [ $? -eq 0 ]; then
     # some error occured, use fallback and reboot directly
     reboot
