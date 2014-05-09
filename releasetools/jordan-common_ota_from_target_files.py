9db35d69e345f63e60b73c37ecbedb705d961ad7
Quarx2k <agent00791@gmail.com>
2014-05-09 16:29:01 +0700

    restore old way copy media_profiles

--- releasetools/jordan-common_ota_from_target_files.py
+++ releasetools/jordan-common_ota_from_target_files.py
@@ -9,9 +9,9 @@ def InstallEnd_SetSpecificDeviceConfigs(self, *args, **kwargs):
 #  self.script.AppendExtra('run_program("/tmp/camera_detect");')
 #  self.script.AppendExtra('ifelse(is_substring("mt9p012", getprop("ro.camera.type")), run_program("/sbin/sh", "-c", "busybox mv /system/etc/media_profiles_mb526.xml /system/etc/media_profiles.xml"));')
 #  self.script.AppendExtra('ifelse(is_substring("camise", getprop("ro.camera.type")), run_program("/sbin/sh", "-c", "busybox mv /system/etc/media_profiles_mb525.xml /system/etc/media_profiles.xml"));')
-  self.script.AppendExtra('package_extract_file("system/etc/media_profiles_mb526.xml", "/system/etc/media_profiles.xml");')
-  self.script.AppendExtra('delete("/system/etc/media_profiles_mb526.xml");')
-  self.script.AppendExtra('delete("/system/etc/media_profiles_mb525.xml");')
+#  self.script.AppendExtra('package_extract_file("system/etc/media_profiles_mb526.xml", "/system/etc/media_profiles.xml");')
+#  self.script.AppendExtra('delete("/system/etc/media_profiles_mb526.xml");')
+ # self.script.AppendExtra('delete("/system/etc/media_profiles_mb525.xml");')
 
 
 def FullOTA_InstallBegin(self, *args, **kwargs):
