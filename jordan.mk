59fd26ff61d38c40e66a2941dbebd7d8b7fd1e37
Quarx2k <agent00791@gmail.com>
2014-05-14 23:01:38 +0700

    add normal dpi to aapt config

--- jordan.mk
+++ jordan.mk
@@ -126,6 +126,6 @@ PRODUCT_PACKAGES += \
 # Should be after the full_base include, which loads languages_full
 PRODUCT_LOCALES := en_US en_GB en_IN fr_FR it_IT de_DE es_ES hu_HU uk_UA zh_CN zh_TW ru_RU nl_NL se_SV cs_CZ pl_PL pt_BR da_DK ko_KR el_GR ro_RO iw_IL ar_EG
 # Include drawables for hdpi densities
-PRODUCT_AAPT_CONFIG := hdpi
+PRODUCT_AAPT_CONFIG := normal hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 
