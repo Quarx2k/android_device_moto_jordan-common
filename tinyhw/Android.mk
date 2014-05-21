f991697fe42168a89917534a43db29c1f64e8e9f
Quarx2k <agent00791@gmail.com>
2014-05-21 17:52:19 +0700

    add symlink to libaudio in Android.mk

--- tinyhw/Android.mk
+++ tinyhw/Android.mk
@@ -34,4 +34,24 @@ LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libdl libex
 
 include $(BUILD_SHARED_LIBRARY)
 
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libaudio.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := FAKE
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): TARGET := /system/lib/audio.primary.omap3.so
+$(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT)/lib/$(LOCAL_MODULE)
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Symlink: $(SYMLINK) -> $(TARGET)"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(SYMLINK))
+	$(hide) rm -rf $@
+	$(hide) rm -rf $(SYMLINK)
+	$(hide) ln -sf $(TARGET) $(SYMLINK)
+	$(hide) touch $@
+
 endif
