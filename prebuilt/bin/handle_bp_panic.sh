00694fa58a8b1bd0ce51935e516054df28b608c0
Quarx2k <agent00791@gmail.com>
2014-06-08 23:38:54 +0700

    fix reboot after bp panic

--- prebuilt/bin/handle_bp_panic.sh
+++ prebuilt/bin/handle_bp_panic.sh
@@ -7,7 +7,7 @@ setprop ctl.stop panic_daemon
 am startservice -a com.cyanogenmod.settings.device.action.HANDLE_BP_PANIC -n com.cyanogenmod.settings.device/.BpPanicHandlerService 2>&1 | grep Error
 touch /cache/bp_panic
 
-if [ $? -eq 0 ]; then
+#if [ $? -eq 0 ]; then
     # some error occured, use fallback and reboot directly
-    reboot
-fi
+#    reboot
+#fi
