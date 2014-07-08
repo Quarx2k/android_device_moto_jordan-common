e634a55be32594e814d058fdb9020ff939e910fb
Quarx2k <agent00791@gmail.com>
2014-07-08 19:05:20 +0700

    Reduce hwui memory usage

--- jordan.mk
+++ jordan.mk
@@ -30,6 +30,20 @@ $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/moto/jordan-common/overlay
 
+#HWUI Tweaks
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.hwui.disable_scissor_opt=true \
+	ro.hwui.texture_cache_size=4 \
+	ro.hwui.fbo_cache_size=4 \
+	ro.hwui.text_small_cache_width=256 \
+	ro.hwui.text_small_cache_height=64 \
+	ro.hwui.text_large_cache_width=512 \
+	ro.hwui.text_large_cache_height=128 \
+	ro.hwui.drop_shadow_cache_size=0.5 \
+	ro.hwui.r_buffer_cache_size=1 \
+	ro.hwui.text_gamma_correction=shader \
+	hwui.use_gpu_pixel_buffers=false
+
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.media.capture.flip=horizontalandvertical \
 	ro.com.google.locationfeatures=1 \
@@ -41,7 +55,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.sf.lcd_density=240 \
 	ro.bq.gpu_to_cpu_unsupported=1 \
 	dalvik.vm.debug.alloc=0 \
-	ro.hwui.disable_scissor_opt=true \
 	cm.updater.uri=http://defy.cm-for.us/api \
 	ro.setupwizard.enable_bypass=1 \
 	persist.sys.root_access=3 \
