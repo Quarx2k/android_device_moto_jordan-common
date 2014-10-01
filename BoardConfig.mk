dfdb18abe41094c03e5d7dd014052e9436c75164
Quarx2k <agent00791@gmail.com>
2014-10-01 19:31:12 +0700

    Switch to new partitions!

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,8 @@ TARGET_NO_PREINSTALL := true
 
 TARGET_BOOTLOADER_BOARD_NAME := jordan
 
+DISABLE_DEXPREOPT =: false
+
 # Board properties
 TARGET_ARCH := arm
 TARGET_BOARD_PLATFORM := omap3
@@ -118,12 +120,11 @@ TW_NO_SCREEN_BLANK := true
 TW_HAS_NO_RECOVERY_PARTITION := true
 TW_HAS_NO_BOOT_PARTITION := true
 TW_NO_SCREEN_TIMEOUT := true
-
-TARGET_RECOVERY_PRE_COMMAND :=  "echo recovery > /cache/recovery/bootmode.conf; sync; \#"
-TARGET_RECOVERY_PRE_COMMAND_CLEAR_REASON := true
-TARGET_NO_SEPARATE_RECOVERY := true
 TW_EXCLUDE_SUPERSU := true
 TW_EXCLUDE_ENCRYPTED_BACKUPS := true
+
+TARGET_RECOVERY_PRE_COMMAND :=  "echo recovery > /bootstrap/bootmode.conf;sync"
+TARGET_NO_SEPARATE_RECOVERY := true
 
 # Egl Specific
 USE_OPENGL_RENDERER := true
@@ -164,7 +165,6 @@ BOARD_HAL_STATIC_LIBRARIES := libhealthd.omap3
 TARGET_PROVIDES_RELEASETOOLS := true
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := build/tools/releasetools/ota_from_target_files --device_specific device/moto/jordan-common/releasetools/jordan-common_ota_from_target_files.py
 TARGET_INCREMENTAL_OTA_VERBATIM_FILES := system/app/Provision.apk
-TARGET_SYSTEMIMAGE_USE_SQUISHER := true
 
 ext_modules:
 	make -C $(TARGET_KERNEL_MODULES_EXT) modules KERNEL_DIR=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=$(TARGET_KERNEL_MODULES_TOOLCHAIN)
@@ -212,14 +212,16 @@ hboot:
 TARGET_KERNEL_SOURCE := $(ANDROID_BUILD_TOP)/jordan-kernel
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 TARGET_KERNEL_MODULES_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilt/linux-x86/toolchain/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-
-BOARD_RECOVERY_KERNEL_CMDLINE := console=/dev/null mem=500M init=/init omapfb.vram=0:4M usbcore.old_scheme_first=y
-BOARD_KERNEL_CMDLINE := $(BOARD_RECOVERY_KERNEL_CMDLINE) panic=30 mmcparts=mmcblk1:p20(kpanic) cpcap_charger_enabled=n
+PARTITION_TABLE := blkdevparts=mmcblk1:51593216@53477376(root_system),140000@396221216(system_sign),523776@396361728(prek1),523776@396886016(prek2),657930240@105070593(system),51200000@763000834(cache),1143996416@814200833(data),4193792@3145728(pds)
+BOARD_RECOVERY_KERNEL_CMDLINE := console=/dev/null mem=500M init=/init omapfb.vram=0:4M usbcore.old_scheme_first=y cpcap_charger_enabled=y $(PARTITION_TABLE)
+BOARD_KERNEL_CMDLINE := console=/dev/null mem=500M init=/init omapfb.vram=0:4M usbcore.old_scheme_first=y cpcap_charger_enabled=n $(PARTITION_TABLE)
+
 # Extra: external modules sources
 TARGET_KERNEL_MODULES_EXT := $(ANDROID_BUILD_TOP)/device/moto/jordan-common/modules/sources/
 
-ifeq ($(TARGET_USE_KERNEL_BACKPORTS),true)
-TARGET_KERNEL_MODULES := ext_modules hboot WLAN_MODULES COMPAT_MODULES
-else
+#ifeq ($(TARGET_USE_KERNEL_BACKPORTS),true)
+#TARGET_KERNEL_MODULES := ext_modules hboot WLAN_MODULES COMPAT_MODULES
+#else
 TARGET_KERNEL_MODULES := ext_modules hboot WLAN_MODULES
-endif
+#endif
 
--- bootstrap/bootstrap.mk
+++ bootstrap/bootstrap.mk
@@ -8,11 +8,8 @@ PRODUCT_COPY_FILES += \
 	${bm_device}/ramdisk/init.usb.rc:root/init.usb.rc \
 	${bm_device}/ramdisk/init.mapphone_umts.rc:root/init.mapphone_umts.rc \
 	${bm_device}/ramdisk/fstab.mapphone_umts:root/fstab.mapphone_umts \
-
-# scripts
-PRODUCT_COPY_FILES += \
-	${bm_device}/bootstrap/script/2nd-boot.sh:system/bootstrap/script/2nd-boot.sh \
-	${bm_device}/bootstrap/script/pdsbackup.sh:system/bootstrap/script/pdsbackup.sh \
+	${bm_device}/ramdisk/badblocks.txt:recovery/root/badblocks.txt \
+	${bm_device}/ramdisk/check.sh:recovery/root/check.sh \
 
 # prebuilt binaries
 PRODUCT_COPY_FILES += \
@@ -25,13 +22,16 @@ PRODUCT_COPY_FILES += \
 	${bm_device}/bootstrap/modules/hbootmod.ko:system/bootstrap/modules/hbootmod.ko \
 	${bm_device}/bootstrap/2nd-boot/hboot.cfg:system/bootstrap/2nd-boot/hboot.cfg \
 	${bm_device}/bootstrap/2nd-boot/hboot_recovery.cfg:system/bootstrap/2nd-boot/hboot_recovery.cfg \
-	${bm_device}/bootstrap/2nd-boot/zImage-recovery:system/bootstrap/2nd-boot/zImage-recovery \
-	$(bm_device)/twrp.fstab:recovery/root/etc/twrp.fstab \
 	${bm_device}/bootstrap/modules/jbd2.ko:system/bootstrap/modules/jbd2.ko \
 	${bm_device}/bootstrap/modules/ext4.ko:system/bootstrap/modules/ext4.ko \
 	${bm_device}/bootstrap/images/background-def.png:system/bootstrap/images/background-def.png \
 	${bm_device}/bootstrap/images/background-blank.png:system/bootstrap/images/background-blank.png \
-	$(OUT)/ramdisk-recovery.img:system/bootstrap/2nd-boot/ramdisk-recovery \
-	$(OUT)/ramdisk.img:system/bootstrap/2nd-boot/ramdisk \
+	${bm_device}/bootstrap/script/2nd-boot.sh:system/bootstrap/script/2nd-boot.sh \
+	$(bm_device)/twrp.fstab:recovery/root/etc/twrp.fstab \
 	$(OUT)/kernel:system/bootstrap/2nd-boot/zImage \
+	$(OUT)/kernel:system/bootstrap/2nd-boot/zImage-recovery \
+	$(OUT)/ramdisk.img:system/bootstrap/2nd-boot/ramdisk \
+	$(OUT)/ramdisk-recovery.img:system/bootstrap/2nd-boot/ramdisk-recovery \
+
+
 
