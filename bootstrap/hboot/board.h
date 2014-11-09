ce5ff65330d39980f3881c5838050f436349a9b8
Quarx2k <agent00791@gmail.com>
2014-11-09 20:46:12 +0600

    Revert "Experimental hboot changes"
    This reverts commit 3dd3e799a1c12242d13f1656a3758e9d9e58d577.

--- bootstrap/hboot/board.h
+++ bootstrap/hboot/board.h
@@ -2,7 +2,6 @@
 #define __BOARD_H__
 
 void board_init();
-void muxing();
 const char *board_get_cmdline();
 
 /* Check defines */
@@ -13,21 +12,6 @@ const char *board_get_cmdline();
 #define BOARD_WDTIMER2_BASE			0x48314000
 #define BOARD_MPU_INTC_BASE			0x48200000
 #define BOARD_GLOBAL_REG_PRM_BASE		0x48307200
-
-/*
- * L4 Peripherals - L4 Wakeup and L4 Core now
- */
-#define OMAP34XX_CORE_L4_IO_BASE	0x48000000
-#define OMAP34XX_WAKEUP_L4_IO_BASE	0x48300000
-#define OMAP34XX_ID_L4_IO_BASE		0x4830A200
-#define OMAP34XX_L4_PER			0x49000000
-#define OMAP34XX_L4_IO_BASE		OMAP34XX_CORE_L4_IO_BASE
-
-/* DMA4/SDMA */
-#define OMAP34XX_DMA4_BASE              0x48056000
-
-/* CONTROL */
-#define OMAP34XX_CTRL_BASE		(OMAP34XX_L4_IO_BASE + 0x2000)
 
 #else
 
