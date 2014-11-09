1fd8c38e0eb3580034acbe5a68f2afa8bcd73ae7
Quarx2k <agent00791@gmail.com>
2014-11-09 23:04:18 +0600

    Fix update bootstrap script

--- bootstrap/script/upd_bootstrap.sh
+++ bootstrap/script/upd_bootstrap.sh
@@ -1,7 +1,9 @@
 #!/sbin/sh
 
-cp -R /system/bootstrap/* /bootstrap/bootstrap/*
-cp -R /system/bootstrap/bootstrap/binary/logwrapper /bootstrap/bin/logwrapper
+cp -R /system/bootstrap/ /bootstrap/
+cp -R /system/bootstrap/binary/logwrapper /bootstrap/bin/logwrapper
 chmod 777 /bootstrap/bin/logwrapper
-chmod 777 /bootstrap/bootstrap/scripts/*
+chmod 777 /bootstrap/bootstrap/script/*
+chmod 777 /bootstrap/bootstrap/binary/*
+chmod 777 /bootstrap/bootstrap/2nd-boot/*
 
